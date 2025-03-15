bec7bc1c893093e682da8c03371344d9afd17dce
Chandra Prakash Kumar <chandraprakash.bhr@gmail.com>
2025-03-15 23:21:21 +0530

    initial commit

---
+++ tests/login-register.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+const randomFigures = Math.random().toString();
+const randomSuffix = randomFigures.substr(10);
+
+test('Register Test', async ({ page }) => {
+  await page.goto('https://parabank.parasoft.com/parabank/register.htm');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await expect(page.getByRole('heading', { name: 'Signing up is easy!' })).toBeVisible();
+  await page.locator('[id="customer\\.firstName"]').fill('Chandra ');
+  await page.locator('[id="customer\\.lastName"]').fill('Kumar');
+  await page.locator('[id="customer\\.address\\.street"]').fill('Pune');
+  await page.locator('[id="customer\\.address\\.city"]').fill('Pune');
+  await page.locator('[id="customer\\.address\\.state"]').fill('Maharastra');
+  await page.locator('[id="customer\\.address\\.zipCode"]').fill('854202');
+  await page.locator('[id="customer\\.phoneNumber"]').fill('88754788896');
+  await page.locator('[id="customer\\.ssn"]').fill('00214');
+  await page.locator('[id="customer\\.username"]').fill(randomSuffix);
+  await page.locator('[id="customer\\.password"]').fill('Munna@2001');
+  await page.locator('#repeatedPassword').fill('Munna@2001');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('heading', { name: 'Welcome '+randomSuffix })).toBeVisible();
+  await expect(page.getByText('Your account was created')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Log Out' })).toBeVisible();
+  await page.getByRole('link', { name: 'Log Out' }).click();
+  await expect(page.getByRole('button', { name: 'Log In' })).toBeVisible();
+  await page.getByRole('paragraph').filter({ hasText: 'Forgot login info?' }).click();
+  await expect(page.getByRole('link', { name: 'Register' })).toBeVisible();
+  await expect(page.locator('input[name="username"]')).toBeVisible();
+  await page.locator('input[name="username"]').fill(randomSuffix);
+  await page.locator('input[name="password"]').fill('Munna@2001');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await expect(page.getByRole('link', { name: 'Log Out' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Accounts Overview' })).toBeVisible();
+});
+
